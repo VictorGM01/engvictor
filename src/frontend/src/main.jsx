161477cf84e420f92537be6a8934260de2fbc963
VictorGM01 <victormarques8801@gmail.com>
2024-02-04 20:32:03 -0300

    feat: define arquitetura do projeto

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes.jsx'
-import './reset.css'
-import './index.css'
+import './styles/reset.scss'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
