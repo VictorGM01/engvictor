28e7b7ee41c98ab877439e844e07db8ae6e8b8a7
VictorGM01 <victormarques8801@gmail.com>
2024-02-04 20:43:24 -0300

    feat: adiciona lógica de rotas das páginas

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes.jsx'
+import AppRoutes from './routes.jsx'
 import './styles/reset.scss'
 import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppRoutes />
   </React.StrictMode>,
 )
--- src/frontend/src/routes.jsx
+++ src/frontend/src/routes.jsx
@@ -1,10 +1,19 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import ProjectsPage from './pages/ProjectsPage/ProjectsPage';
+import ContactPage from './pages/ContactPage/ContactPage';
 
+function AppRoutes() {
   return (
-    <>
-      <h1>Olá</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default AppRoutes;
