001a0a9556053104c60e8a6d5220a6225833c412
VictorGM01 <victormarques8801@gmail.com>
2024-02-05 16:18:59 -0300

    feat: carrega experiências dinamicamente

--- src/frontend/src/sections/Experience/Experience.jsx
+++ src/frontend/src/sections/Experience/Experience.jsx
@@ -1,71 +1,33 @@
 import styles from './Experience.module.scss'
 import Link from '../../components/common/Link/Link'
-import v2 from '../../assets/images/v2.png'
-import inai from '../../assets/images/inai.png'
-import gazeta from '../../assets/images/gazeta.png'
-import mobly from '../../assets/images/mobly.png'
+import experiences from '../../data/experiences/experiences.json'
 
 export default function Experience() {
+
+  const geraItens = () => {
+    return experiences.map((experience) => (
+      <li key={experience.id}>
+        <input type="checkbox" id={`check-${experience.id}`} />
+        <label htmlFor={`check-${experience.id}`}>
+        <div>
+          <img src={experience.logo} alt={experience.empresa} />
+          <p>
+            {experience.empresa}
+          </p>
+        </div>
+        <article className={styles.experience__card}>
+          {experience.descricao}
+        </article>
+        </label>
+      </li>
+    ))}
+
   return (
     <section className={styles.experience}>
       <Link nome="Experiências" />
       <div className={styles.experience__container}>
         <ul>
-          <li>
-            <div>
-              <img src={v2} alt="V2 Consulting" />
-              <p>
-                V2 Consulting
-              </p>
-              <article>
-
-              </article>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img src={inai} alt="Inai" />
-              <p>
-                InAi
-              </p>
-              <article>
-
-              </article>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img src={mobly} alt="Mobly" />
-              <p>
-                Mobly
-              </p>
-              <article>
-
-              </article>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img src={gazeta} alt="Gazeta" />
-              <p>
-                Rede Gazeta
-              </p>
-              <article>
-
-              </article>
-            </div>
-          </li>
-          <li>
-            <div>
-              <img src={v2} alt="V2 Consulting" />
-              <p>
-                V2 Consulting
-              </p>
-              <article>
-
-              </article>
-            </div>
-          </li>
+          {geraItens()}
         </ul>
       </div>
     </section>
