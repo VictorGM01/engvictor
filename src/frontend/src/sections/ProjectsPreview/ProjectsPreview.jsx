e7aff5fe8b0828958061e9244a174f1c183d6692
VictorGM01 <victormarques8801@gmail.com>
2024-02-10 02:35:52 -0300

    feat: ajusta seção de projetos para desk e tablet

--- src/frontend/src/sections/ProjectsPreview/ProjectsPreview.jsx
+++ src/frontend/src/sections/ProjectsPreview/ProjectsPreview.jsx
@@ -9,9 +9,11 @@ export default function ProjectsPreview() {
   return (
     <section className={styles.projectsPreview} id="projects">
       <LinkSection nome='Projects'/>
+      <div className={styles.projectsPreview__item}>
       {projects.map((project, index) => (
         <CardProject key={index} project={project} />
       ))}
+      </div>
       <Link to='/projects' className={styles.projectsPreview__link}>Ver mais</Link>
     </section>
   );
