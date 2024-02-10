399bec604f7429eee048296265aacc0555f8bf92
VictorGM01 <victormarques8801@gmail.com>
2024-02-10 00:02:08 -0300

    feat: ajusta seção de sobre para desk e tablet

--- src/frontend/src/sections/About/About.jsx
+++ src/frontend/src/sections/About/About.jsx
@@ -11,25 +11,34 @@ export default function About() {
   return (
     <section className={styles.about} id="about">
       <Link nome="Sobre" />
-
+      <img src="/grafico.svg" alt="Gráfico" className={styles.about__grafico} />
       <div className={styles.about__container}>
-        <img src={Capa} alt="Foto de perfil" />
-        <p className={styles.about__nome}>Victor Gabriel Marques</p>
-        <div className={styles.about__links}>
-          <a href="mailto:victor@engvictor.com.br">
-            <img src={Email} alt="Email" />
-          </a>
-          <a href="https://github.com/VictorGM01">
-            <img src={Github} alt="Github" />
-          </a>
-          <a href="https://www.linkedin.com/in/victor-gabriel-marques/">
-            <img src={Linkedin} alt="Linkedin" />
-          </a>
-        </div>
+        <section className={styles.about__container_perfil}>
+          <img src={Capa} alt="Foto de perfil" />
+          <div className={styles.about__links}>
+            <p className={styles.about__nome}>Victor Gabriel Marques</p>
+            <div>
+              <a href="mailto:victor@engvictor.com.br">
+                <img src={Email} alt="Email" />
+              </a>
+              <a href="https://github.com/VictorGM01">
+                <img src={Github} alt="Github" />
+              </a>
+              <a href="https://www.linkedin.com/in/victor-gabriel-marques/">
+                <img src={Linkedin} alt="Linkedin" />
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className={styles.about__container_sobre}>
         <p className={styles.about__sobre}>
           <span>Engenheiro de Software</span> e entusiasta da <span>filosofia do código limpo</span> (Clean Code). Minha experiência profissional inclui o desenvolvimento de <span>aplicações web completas</span>, incluindo back-end, front-end e dados. Possuo conhecimento prático em Python, Django, JavaScript, TypeScript, Node.JS, AdonisJS, e trabalho com bancos de dados como MySQL, PostgreSQL e SQL Server. Além disso, tenho experiência com <span>tecnologias front-end</span>, incluindo HTML5, CSS3, e ReactJS.
         </p>
+
         <Button nome="Entre em contato" link="#contact" />
+        </section>
+        
+        
       </div>
     </section>
   )
