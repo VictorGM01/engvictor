b8ed5845306fd84043d3b450827972952a3cdf36
VictorGM01 <victormarques8801@gmail.com>
2024-02-10 01:15:04 -0300

    feat: ajusta seção de experiencias para desk e tablet

--- src/frontend/src/sections/Experience/Experience.jsx
+++ src/frontend/src/sections/Experience/Experience.jsx
@@ -1,34 +1,45 @@
-import styles from './Experience.module.scss'
-import Link from '../../components/common/Link/Link'
-import experiences from '../../data/experiences/experiences.json'
+import { useState } from 'react';
+import styles from './Experience.module.scss';
+import Link from '../../components/common/Link/Link';
+import experiences from '../../data/experiences/experiences.json';
 
 export default function Experience() {
+  const [openItemId, setOpenItemId] = useState(1);
+
+  const handleItemClick = (id) => {
+    setOpenItemId(openItemId === id ? null : id);
+  };
 
   const geraItens = () => {
     return experiences.map((experience) => (
-      <li key={experience.id}>
-        <input type="checkbox" id={`check-${experience.id}`} />
-        <label htmlFor={`check-${experience.id}`}>
-        <div>
+      <li key={experience.id} className={styles.experience__item}>
+        <div
+          onClick={() => handleItemClick(experience.id)}
+          className={`${styles.experience__header} ${openItemId === experience.id ? styles.experience__highlight : ''}`}
+        >
           <img src={experience.logo} alt={experience.empresa} />
-          <p>
-            {experience.empresa}
-          </p>
+          <p>{experience.empresa}</p>
         </div>
-        <article className={styles.experience__card}>
-          <h1>{experience.cargo}</h1>
-          <p>{experience.dataInicio} - {experience.dataFim}</p>
-          <p className={styles.experience__desc}>{experience.descricao}</p>
-          <p>Tecnologias utilizadas:</p>
-          <section>
-            {experience.tecnologias.map((tecnologia) => (
-              <img src={`/${tecnologia}.svg`} alt={tecnologia} key={tecnologia} />
-            ))}
-          </section>
-        </article>
-        </label>
+        {openItemId === experience.id && (
+          <article className={styles.experience__card}>
+            <div style={{ display: "block", width: "100%" }}>
+              <h1>{experience.cargo}</h1>
+              <p>{experience.dataInicio} - {experience.dataFim}</p>
+            </div>
+            <p className={styles.experience__desc}>{experience.descricao}</p>
+            <span>
+              <p>Tecnologias utilizadas:</p>
+              <section>
+                {experience.tecnologias.map((tecnologia) => (
+                  <img src={`/${tecnologia}.svg`} alt={tecnologia} key={tecnologia} title={`logo da tecnologia: ${tecnologia}`} />
+                ))}
+              </section>
+            </span>
+          </article>
+        )}
       </li>
-    ))}
+    ));
+  };
 
   return (
     <section className={styles.experience} id="experience">
@@ -39,5 +50,5 @@ export default function Experience() {
         </ul>
       </div>
     </section>
-  )
+  );
 }
