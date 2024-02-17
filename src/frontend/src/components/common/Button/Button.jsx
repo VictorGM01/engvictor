aa691f89147fac2ba7b3cf5930393f5af74987c8
VictorGM01 <victormarques8801@gmail.com>
2024-02-17 17:08:52 -0300

    refactor: torna button flexível à link e submit

--- src/frontend/src/components/common/Button/Button.jsx
+++ src/frontend/src/components/common/Button/Button.jsx
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
 import styles from './Button.module.scss'
 
-export default function Button({ nome, link }) {
+export default function Button({ nome, link, button = false, type = 'button'}) {
   return (
-    <a href={link} className={styles.button}>
-      {nome}
-    </a>
+    button ? (
+      <button type={type} className={styles.button}>
+        {nome}
+      </button>
+    ) : (
+      <a href={link} className={styles.button}>
+        {nome}
+      </a>
+    )
   )
 }
 
