671f3233b527fea6c4283e449a291169fc9fe92f
VictorGM01 <victormarques8801@gmail.com>
2024-02-18 22:20:28 -0300

    feat: adiciona botão de voltar

--- src/frontend/src/sections/Projects/Projects.jsx
+++ src/frontend/src/sections/Projects/Projects.jsx
@@ -2,11 +2,13 @@ import styles from './Projects.module.scss';
 import Link from '../../components/common/Link/Link';
 import projects from '../../data/projects/projects.json';
 import CardProject from '../../components/common/CardProject/CardProject';
+import GoBack from "../../components/common/GoBack/GoBack";
 
 
 export default function Projects() {
   return (
     <section className={styles.projects}>
+      <GoBack />
       <Link nome='Projetos' />
       <hr />
       <div className={styles.projects__container}>
