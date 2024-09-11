c6f367c4c1d0eec779431ee1d1ee07d700d72d5d
VictorGM01 <victormarques8801@gmail.com>
2024-09-10 21:41:36 -0300

    refactor: atualiza estilo da seção de Sobre

--- src/frontend/src/components/common/Button/Button.jsx
+++ src/frontend/src/components/common/Button/Button.jsx
@@ -1,23 +1,26 @@
-import PropTypes from 'prop-types';
-import styles from './Button.module.scss'
+import PropTypes from "prop-types";
+import styles from "./Button.module.scss";
 
-export default function Button({ nome, link, button = false, type = 'button'}) {
-  return (
-    button ? (
-      <button type={type} className={styles.button}>
-        {nome}
-      </button>
-    ) : (
-      <a href={link} className={styles.button}>
-        {nome}
-      </a>
-    )
-  )
+export default function Button({
+  nome,
+  link,
+  button = false,
+  type = "button",
+}) {
+  return button ? (
+    <button type={type} className={styles.button}>
+      {nome}
+    </button>
+  ) : (
+    <a href={link} className={`${styles.button} ${styles["button-link"]}`}>
+      {nome}
+    </a>
+  );
 }
 
 Button.propTypes = {
   nome: PropTypes.string.isRequired,
   link: PropTypes.string,
   button: PropTypes.bool,
-  type: PropTypes.string
-}
+  type: PropTypes.string,
+};
--- src/frontend/src/sections/About/About.jsx
+++ src/frontend/src/sections/About/About.jsx
@@ -1,11 +1,10 @@
-import Link from "../../components/common/Link/Link"
-import styles from './About.module.scss'
-import Capa from '../../assets/images/capa.png'
-import Email from '../../assets/icons/email.png'
-import Github from '../../assets/icons/github.png'
-import Linkedin from '../../assets/icons/linkedin.png'
-import Button from "../../components/common/Button/Button"
-
+import Link from "../../components/common/Link/Link";
+import styles from "./About.module.scss";
+import Capa from "../../assets/images/capa.png";
+import Email from "../../assets/icons/email.svg";
+import Github from "../../assets/icons/github.svg";
+import Linkedin from "../../assets/icons/linkedin.svg";
+import Button from "../../components/common/Button/Button";
 
 export default function About() {
   return (
@@ -18,28 +17,49 @@ export default function About() {
           <div className={styles.about__links}>
             <p className={styles.about__nome}>Victor Gabriel Marques</p>
             <div>
-              <a href="mailto:victor@engvictor.com.br">
-                <img src={Email} alt="Email" />
-              </a>
-              <a href="https://github.com/VictorGM01">
+              <a
+                href="https://github.com/VictorGM01"
+                target="_blank"
+                rel="noreferrer"
+                title="Github"
+              >
                 <img src={Github} alt="Github" />
               </a>
-              <a href="https://www.linkedin.com/in/victor-gabriel-marques/">
+              <a
+                href="mailto:victor@engvictor.com.br"
+                target="_blank"
+                rel="noreferrer"
+                title="Email"
+              >
+                <img src={Email} alt="Email" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/victor-gabriel-marques/"
+                target="_blank"
+                rel="noreferrer"
+                title="Linkedin"
+              >
                 <img src={Linkedin} alt="Linkedin" />
               </a>
             </div>
           </div>
         </section>
         <section className={styles.about__container_sobre}>
-        <p className={styles.about__sobre}>
-          <span>Engenheiro de Software</span> e entusiasta da <span>filosofia do código limpo</span> (Clean Code). Minha experiência profissional inclui o desenvolvimento de <span>aplicações web completas</span>, incluindo back-end, front-end e dados. Possuo conhecimento prático em Python, Django, JavaScript, TypeScript, Node.JS, AdonisJS, e trabalho com bancos de dados como MySQL, PostgreSQL e SQL Server. Além disso, tenho experiência com <span>tecnologias front-end</span>, incluindo HTML5, CSS3, e ReactJS.
-        </p>
+          <p className={styles.about__sobre}>
+            <span>Engenheiro de Software</span> e entusiasta da{" "}
+            <span>filosofia do código limpo</span> (Clean Code). Minha
+            experiência profissional inclui o desenvolvimento de{" "}
+            <span>aplicações web completas</span>, incluindo back-end, front-end
+            e dados. Possuo conhecimento prático em Python, Django, JavaScript,
+            TypeScript, Node.JS, AdonisJS, e trabalho com bancos de dados como
+            MySQL, PostgreSQL e SQL Server. Além disso, tenho experiência com{" "}
+            <span>tecnologias front-end</span>, incluindo HTML5, CSS3, e
+            ReactJS.
+          </p>
 
-        <Button nome="Entre em contato" link="#contact" />
+          <Button nome="Entre em contato" link="#contact" />
         </section>
-        
-        
       </div>
     </section>
-  )
+  );
 }
